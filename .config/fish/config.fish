c4f113797da95303c9ffb20c0d3b56b0e9891027
Arimil <renari@arimil.com>
2025-12-14 22:59:20 -0500

    fix fastfetch with fastfetch changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ fenv source ~/.profile
 # overwrite greeting
 function fish_greeting
     sleep 0.1
-    fastfetch --kitty ~/.config/fastfetch/Character_Silver_Wolf_Splash_Art-512.png
+    fastfetch --logo ~/.config/fastfetch/Character_Silver_Wolf_Splash_Art-512.png
 end
 
 # workaround for ghostty issues with ssh
