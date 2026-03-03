926655a13358dcb89edc551add25ff33e6dbf90e
Arimil <renari@arimil.com>
2026-03-03 04:06:21 -0500

    add bitwarden ssh agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,4 +11,7 @@ end
 # workaround for ghostty issues with ssh
 alias ssh="TERM=xterm-256color /usr/bin/ssh"
 
+# bitwarden ssh agent
+test -S $HOME/.bitwarden-ssh-agent.sock; and set -gx SSH_AUTH_SOCK $HOME/.bitwarden-ssh-agent.sock
+
 [ -s "/home/arimil/.jabba/jabba.fish" ]; and source "/home/arimil/.jabba/jabba.fish"
